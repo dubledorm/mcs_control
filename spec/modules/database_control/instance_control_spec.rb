0c9a30d15402b384c4442c901e09df8b3a58fc8d
Aleksey Aleshin <dubledorm@yandex.ru>
2019-07-02 13:13:33 +0300

    Добавлен класс InstanceControl

--- spec/modules/database_control/instance_control_spec.rb
+++ spec/modules/database_control/instance_control_spec.rb
@@ -16,6 +16,7 @@ describe DatabaseControl::InstanceControl do
 
     it 'should create instance' do
       expect {
+        # noinspection SpellCheckingInspection
         instance_control = DatabaseControl::InstanceControl.new('test-milandr-chicken')
         instance_control.prepare
         instance_control.call
@@ -24,18 +25,41 @@ describe DatabaseControl::InstanceControl do
 
 
     it 'should create right user name' do
+      # noinspection SpellCheckingInspection
       instance_control = DatabaseControl::InstanceControl.new('test-milandr-chicken')
       instance_control.prepare
       instance_control.call
+      # noinspection SpellCheckingInspection
       expect(instance_control.instance.db_user_name).to eq('test_milandr_chicken')
     end
 
 
     it 'should create database user' do
+      # noinspection SpellCheckingInspection
       instance_control = DatabaseControl::InstanceControl.new('test-milandr-chicken')
       instance_control.prepare
       instance_control.call
+      # noinspection SpellCheckingInspection
       expect(get_database_users_list(ActiveRecord::Base.connection).include?('test_milandr_chicken')).to be(true)
     end
   end
+
+  describe 'database user already exists' do
+    before :each do
+      # noinspection SpellCheckingInspection,SpellCheckingInspection
+      create_user(ActiveRecord::Base.connection, 'test_milandr_chicken', 'test_milandr_chicken')
+    end
+
+    it 'should create database user' do
+      # noinspection SpellCheckingInspection
+      instance_control = DatabaseControl::InstanceControl.new('test-milandr-chicken')
+      instance_control.prepare
+      instance_control.call
+      # noinspection SpellCheckingInspection
+      expect(get_database_users_list(ActiveRecord::Base.connection).include?('test_milandr_chicken_1')).to be(true)
+    end
+  end
+
+
+
 end
