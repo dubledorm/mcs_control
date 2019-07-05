a7870a4c2566d78e420c299d507aa5be9d3b4231
Aleksey Aleshin <dubledorm@yandex.ru>
2019-07-05 17:47:41 +0300

    Перенесён сервис decide_on_db_user_service.rb в DatabaseControl

--- lib/instance/database_control/db_prepare.rb
+++ lib/instance/database_control/db_prepare.rb
@@ -10,7 +10,7 @@ class Instance
   #    DB_USER_PASSWORD_LENGTH = 8
 
       def self.build(instance)
-        instance.db_user_name = Instance::DecideOnDbUserService.new(instance).call
+        instance.db_user_name = Instance::DatabaseControl::DecideOnDbUserService.new(instance).call
         instance.db_user_password = GeneratePassword::generate_password(8)
         create_user(ActiveRecord::Base.connection, instance.db_user_name, instance.db_user_password)
         instance.db_status = 'everywhere_exists'
