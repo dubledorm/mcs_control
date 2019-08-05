bd49c943129f3a1430f76b8f423015abf6c18b4a
Aleksey Aleshin <dubledorm@yandex.ru>
2019-08-05 18:11:05 +0300

    Тест для Port #Create

--- app/admin/port.rb
+++ app/admin/port.rb
@@ -16,16 +16,18 @@ ActiveAdmin.register Port do
   end
 
   controller do
+    include ApplicationHelper
 
     def create
       begin
         flash.delete(:alert)
         @resource = Port.new(params.require(:port).permit(:number, :program_id, :port_type))
         Port::Factory::build(@resource)
+        test_point_exception
         redirect_to admin_instance_program_path(id: @resource.program_id, instance_id: @resource.program.instance_id)
       rescue StandardError => e
-        flash[:alert] = e.message
-        redirect_to new_admin_program_port_path(program_id: params.require(:port)[:program_id]), alert: e.message
+        flash[:error] = I18n.t('activerecord.errors.messages.unknown_resource_exception', errors: e.message)
+        redirect_to new_admin_program_port_path(program_id: params.require(:port)[:program_id]), error: e.message
       end
     end
   end
