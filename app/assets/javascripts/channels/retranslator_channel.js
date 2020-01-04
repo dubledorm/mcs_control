75390d778144749ac78a4a9b4b570771a38eb666
Aleksey Aleshin <dubledorm@yandex.ru>
2020-01-04 23:41:05 +0300

    Отредакитрован retranslator_channel.js

--- app/assets/javascripts/channels/retranslator_channel.js
+++ app/assets/javascripts/channels/retranslator_channel.js
@@ -1,19 +1,47 @@
 //import consumer from "../consumer"
 
-App.cable.subscriptions.create("RetranslatorChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+// App.cable.subscriptions.create("RetranslatorChannel", {
+//   connected() {
+//     // Called when the subscription is ready for use on the server
+//   },
+//
+//   disconnected() {
+//     // Called when the subscription has been terminated by the server
+//   },
+//
+//   received(data) {
+//     // Called when there's incoming data on the websocket for this channel
+//   },
+//
+//   trace: function() {
+//     return this.perform('trace');
+//   }
+// });
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  },
+$(function() {
+    $('[data-channel-subscribe="Retranslator"]').each(function(index, element) {
+        var $element = $(element),
+            room_id = $element.data('room-id')
+        messageTemplate = $('[data-role="message-template"]');
 
-  trace: function() {
-    return this.perform('trace');
-  }
+        $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000)
+
+        App.cable.subscriptions.create(
+            {
+                channel: "Retranslator",
+                room: "3001"
+            },
+            {
+                received: function(data) {
+                    var content = messageTemplate.children().clone(true, true);
+                   // content.find('[data-role="user-avatar"]').attr('src', data.user_avatar_url);
+                    content.find('[data-role="message-text"]').text(data.message);
+                   // content.find('[data-role="message-date"]').text(data.updated_at);
+                    $element.append(content);
+                    $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000);
+                }
+            }
+        );
+    });
 });
